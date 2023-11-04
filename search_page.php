5b3c2274f508cb87a63c485706168344d223775f
manhdung <dungli1221@gmail.com>
2023-11-04 10:21:43 +0700

    init final

--- search_page.php
+++ search_page.php
@@ -82,13 +82,14 @@
                while($fetch_products = mysqli_fetch_assoc($select_products)){
       ?>
                   <form action="" method="post" class="box">
-                     <img class="image" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
-                     <div class="name"><?php echo $fetch_products['name']; ?> (<?php echo $fetch_products['category']; ?>)</div>
-                     <div class="name"><?php echo $fetch_products['describes']; ?></div>
-                     <div class="price"><?php echo $fetch_products['newprice']; ?>/<span style="text-decoration-line:line-through"><?php echo $fetch_products['price']; ?></span> VND (<?php echo $fetch_products['discount']; ?>% SL: <?php echo $fetch_products['quantity']; ?>)</div>
+                     <img  width="207px" height="224px" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
+                     <div class="name"><?php echo $fetch_products['name']; ?></div>
+                     <div class="sub-name">Thương hiệu: <?php echo $fetch_products['trademark']; ?></div>
+                     <div class="sub-name">Mô tả: <?php echo $fetch_products['describes']; ?></div>
+                     <div class="price"><?php echo number_format($fetch_products['newprice'],0,',','.' ); ?>/<span style="text-decoration-line:line-through; text-decoration-thickness: 2px; text-decoration-color: grey"><?php echo number_format($fetch_products['price'],0,',','.' ); ?></span> VND (<?php echo $fetch_products['discount']; ?>% SL: <?php echo $fetch_products['quantity']; ?>)</div>
+                     <span style="font-size: 17px; display: flex;">Số lượng mua:</span>
                      <input type="number" min="<?=($fetch_products['quantity']>0) ? 1:0 ?>" max="<?php echo $fetch_products['quantity']; ?>" name="product_quantity" value="<?=($fetch_products['quantity']>0) ? 1:0 ?>" class="qty">
                      <input type="hidden" name="product_name" value="<?php echo $fetch_products['name']; ?>">
-                     <input type="hidden" name="product_category" value="<?php echo $fetch_products['category']; ?>">
                      <input type="hidden" name="product_price" value="<?php echo $fetch_products['newprice']; ?>">
                      <input type="hidden" name="product_image" value="<?php echo $fetch_products['image']; ?>">
                      <input type="submit" value="Thêm vào giỏ hàng" name="add_to_cart" class="btn">
--- shop.php
+++ shop.php
@@ -141,7 +141,7 @@
                         <img width="207px" height="224px" src="uploaded_img/<?php echo $fetch_products['image']; ?>" alt="">
                         <div class="name"><?php echo $fetch_products['name']; ?></div>
                         <div class="sub-name">Thương hiệu: <?php echo $fetch_products['trademark']; ?></div>
-                           <div class="sub-name">Mô tả: <?php echo $fetch_products['describes']; ?></div>
+                        <div class="sub-name">Mô tả: <?php echo $fetch_products['describes']; ?></div>
                         <div class="price"><?php echo number_format($fetch_products['newprice'],0,',','.' ); ?>/<span style="text-decoration-line:line-through; text-decoration-thickness: 2px; text-decoration-color: grey"><?php echo number_format($fetch_products['price'],0,',','.' ); ?></span> VND (<?php echo $fetch_products['discount']; ?>% SL: <?php echo $fetch_products['quantity']; ?>)</div>
                         <span style="font-size: 17px; display: flex;">Số lượng mua:</span>
                         <input type="number" min="<?=($fetch_products['quantity']>0) ? 1:0 ?>" max="<?php echo $fetch_products['quantity']; ?>" name="product_quantity" value="<?=($fetch_products['quantity']>0) ? 1:0 ?>" class="qty">
